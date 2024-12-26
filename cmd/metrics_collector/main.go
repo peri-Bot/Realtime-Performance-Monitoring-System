6ce406d8336caa3b6abad3564e421c11600692c2
Kidus Abebe <kidsebk@gmail.com>
2024-12-26 23:28:13 +0300

    add: print metrices

--- cmd/metrics_collector/main.go
+++ cmd/metrics_collector/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
-	"github.com/peri-Bot/Realtime-Performance-Monitoring-System/internal/metrics"
+	"log"
 	"time"
+
+	"github.com/peri-Bot/Realtime-Performance-Monitoring-System/internal/metrics"
 )
 
 func main() {
 	for {
-		metrics.CollectMem()
+		metric, err := metrics.CollectMetrics()
+		if err != nil {
+			log.Println("Error collecting metrics:", err)
+			continue
+		}
+
+		metrics.PrintMetrics(metric)
+
 		time.Sleep(5 * time.Second) // Collect metrics every 5 seconds
 	}
 }
