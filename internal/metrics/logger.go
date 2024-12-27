22076dcb342dd4fdb1f6651e8e30f3dc4a092f06
Kidus Abebe <kidsebk@gmail.com>
2024-12-27 18:09:33 +0300

    add: metric logging

---
+++ internal/metrics/logger.go
@@ -0,0 +1,17 @@
+package metrics
+
+import (
+	"encoding/json"
+	"log"
+)
+
+// LogMetrics logs metrics as a JSON string.
+func LogMetrics(metrics *Metrics) {
+	processedMetrics := ProcessMetrics(metrics)
+	data, err := json.MarshalIndent(processedMetrics, "", "  ")
+	if err != nil {
+		log.Println("Error serializing metrics:", err)
+		return
+	}
+	log.Println(string(data))
+}
