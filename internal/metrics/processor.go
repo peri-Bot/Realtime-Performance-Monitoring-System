78f632809df86439247268385879b138101a7b1b
Kidus Abebe <kidsebk@gmail.com>
2024-12-27 18:08:59 +0300

    add: Metric data processing

--- internal/metrics/processor.go
+++ internal/metrics/processor.go
@@ -1,1 +1,20 @@
 package metrics
+
+func ProcessMetrics(metrics *Metrics) map[string]interface{} {
+	return map[string]interface{}{
+		"cpu_usage": metrics.CPU,
+		"memory": map[string]interface{}{
+			"total": metrics.Memory.Total,
+			"used":  metrics.Memory.Used,
+			"free":  metrics.Memory.Free,
+			"usage": metrics.Memory.Usage,
+		},
+		"disk": map[string]interface{}{
+			"total": metrics.Disk.Total,
+			"used":  metrics.Disk.Used,
+			"free":  metrics.Disk.Free,
+			"usage": metrics.Disk.Usage,
+		},
+		"timestamp": metrics.Timestamp,
+	}
+}
